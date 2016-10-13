8dd2af579513e956dab7865c2a92e4850a1e325b
hansellesteban <hansell.ramos@gmail.com>
2016-10-12 20:23:40 -0500

    Added query and error handling

--- Nube-S1-ISBNQuery/ViewController.swift
+++ Nube-S1-ISBNQuery/ViewController.swift
@@ -7,9 +7,31 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtISBN: UITextField!
+    @IBOutlet weak var txvResultado: UITextView!
+    @IBAction func btnBuscar(_ sender: UIButton) {
+        txvResultado.text = "Cargando..."
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(txtISBN.text!)"
+        let url = URL(string: urls)
+        let urlRequest = URLRequest(url: url!)
+        let session = URLSession.shared
+        let dt = session.dataTask(with: urlRequest, completionHandler: { (datos, resp, error) in
+            if error == nil {
+                let texto = NSString(data: datos!, encoding: String.Encoding.utf8.rawValue)
+                DispatchQueue.main.sync(){
+                    self.txvResultado.text = texto! as String
+                }
+            }else{
+                DispatchQueue.main.sync(){
+                    self.txvResultado.text = "Error!"
+                }
+            }
+        })
+        
+        dt.resume()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
